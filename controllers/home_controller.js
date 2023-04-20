2915176d6d8efdd65fec78092b429c4d82264ec7
Sachin <prajapatsachin7078@gmail.com>
2023-04-20 20:59:33 +0530

    setup static files for layout and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,4 @@
 module.exports.home = function(req, res) {
-    // return res.send('<h1>Express is up for Codial!</h1>');
     // rendering the content to the screen
     return res.render('home',{
         title:"home"
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+
+// require express-ejs-layouts
+const expressLayout = require('express-ejs-layouts');
+
+
 const app = express();
 const port = 8000;
+app.use(express.static('./assets'));
+// extracting styles and script from the sub pages into layout files
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+// use express-ejs-layouts
+app.use(expressLayout);
 
 // use ejs as a view engine
 app.set('view engine', 'ejs');
