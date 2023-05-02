e5e88b9c7c7a38b81b4b8dfc23761888ad8343e6
Sachin <prajapatsachin7078@gmail.com>
2023-05-02 08:55:06 +0530

    sign-up created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
+// Importing the Express library
 const express = require('express');
 
+// Creating a router instance
 const router = express.Router();
 
+// Importing the user_controller
 const usersController = require('../controllers/user_controller');
 
-router.get('/profile',usersController.profile);
+// Defining the routes and the respective controller functions
+router.get('/profile', usersController.profile); // Route for user profile
+router.get('/sign-up', usersController.signUp); // Route for user sign-up page
+router.get('/sign-in', usersController.signIn); // Route for user sign-in page
 
+router.post('/create', usersController.create); // Route for submitting the sign-up form
+router.post('/create-session', usersController.createSession); // Route for submitting the sign-in form and creating a new session
+
+// Exporting the router module
 module.exports = router;
