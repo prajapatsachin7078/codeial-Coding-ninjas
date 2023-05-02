380464894ca9ae8a36353ed539faf61aac9c0796
Sachin <prajapatsachin7078@gmail.com>
2023-05-02 09:09:05 +0530

    models user schema added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,98 @@
-module.exports.profile = function(req,res){
+const User = require('../models/user');
 
-    return res.render('user_profile',{
-        title:'Profile'
+// Render user profile if user is logged in
+module.exports.profile = function(req, res){
+    // Check if user is logged in
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id)
+        .then(function(user){
+            // If user is found, render user profile page
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                });
+            }
+            // If user not found, redirect to sign in page
+            return res.redirect('/users/sign-in');
+        })
+        .catch(function(err){
+            console.error('Error in finding user', err);
+            return res.redirect('/users/sign-in');
+        });
+    } else {
+        // If user not logged in, redirect to sign in page
+        return res.redirect('/users/sign-in');
+    }
+};
+
+// Render sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Codeial | Sign Up'
+    });
+};
+
+// Render sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Codeial | Sign In'
+    });
+};
+
+// Create new user with sign up data from browser
+module.exports.create = function(req, res){
+    // Check if password matches confirm password
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    // Find user with email
+    User.findOne({email: req.body.email})
+    .then(function(user){
+        // If user not found, create new user and redirect to sign in page
+        if(!user){
+            User.create(req.body)
+                .then(function(user){
+                    return res.redirect('/users/sign-in')
+                })
+                .catch(function(err){
+                    console.error('Error in creating user', err);
+                    return;
+                })
+        } else {
+            // If user found, redirect to sign up page
+            return res.redirect('back');
+        }
+    })
+    .catch(function(err){
+        console.error('Error in finding user', err);
+        return;
+    })
+};
+
+// Create new session for user upon sign in
+module.exports.createSession = function(req, res){
+    // Find user with email
+    User.findOne({email:req.body.email})
+    .then(function(user){
+        // Handle success/User found
+        if(user){
+            // Handle incorrect password
+            if(req.body.password != user.password){
+                return res.redirect('back');
+            }
+            // Handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }
+        // Handle user not found
+        else{
+            return res.redirect('back');
+        }
+    })
+    .catch(function(err){
+        console.error('Error in signing in the user',err);
+        return;
     });
 };
--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+// Importing required modules
 const express = require('express');
-
-// require express-ejs-layouts
+const cookieParser = require('cookie-parser');
 const expressLayout = require('express-ejs-layouts');
+const db = require('./config/mongoose'); 
 
-
-const app = express();
+// Setting the port number
 const port = 8000;
-app.use(express.static('./assets'));
-// extracting styles and script from the sub pages into layout files
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
 
-// use express-ejs-layouts
+// Creating an instance of the express server
+const app = express();
+
+// Using the body-parser and cookie-parser middleware
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+// Using the static file middleware to serve static files
+app.use(express.static('./assets'));
+
+// Extracting styles and scripts from the sub-pages into layout files
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// Using the express-ejs-layouts middleware
 app.use(expressLayout);
 
-// use ejs as a view engine
+// Setting the view engine and views folder
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-app.set('views','./views');
+app.set('views', './views');
 
-// use express router
-app.use('/',require('./routes/'));
+// Using the express router and importing the routes from the routes folder
+const router = require('./routes/');
+app.use('/', router);
 
+// Starting the server and listening to the specified port number
 app.listen(port, function(err){
     if(err){
-        console.error(`Error in runnig the server on ${port} ${err.message}`);
+        console.error(`Error in running the server on port ${port} ${err.message}`);
     }
     console.log(`Listening on ${port}`);
-})
+});
